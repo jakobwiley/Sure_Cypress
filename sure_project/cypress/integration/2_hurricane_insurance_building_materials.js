7e1f7e709dc783d65d5716812bc5fff33125cfad
jakobwiley <jakobwiley@gmail.com>
2021-08-18 20:58:58 -0500

    Finished persisting test cases

--- sure_project/cypress/integration/2_hurricane_insurance_building_materials.js
+++ sure_project/cypress/integration/2_hurricane_insurance_building_materials.js
@@ -18,16 +18,16 @@ describe('Hurricane Insurance Building Materials page', () => {
     it('Customer clicks Next without selecting a building material option', () => {
     //Next button dooesn't have a disabled UI or UX state. SURE-28 JIRA TICKET.
         cy.contains('Next').click()
-        cy.get(':nth-child(1) > .MuiButtonBase-root > .MuiIconButton-label > .jss10').should('not.be.checked') //Confirming Straw radio is not checked
-        cy.get(':nth-child(2) > .MuiButtonBase-root > .MuiIconButton-label > .jss10').should('not.be.checked') //Confirming Sticks radio is not checked
-        cy.get(':nth-child(3) > .MuiButtonBase-root > .MuiIconButton-label > .jss10').should('not.be.checked') //Confirming Bricks radio is not checked
+        cy.get(':nth-child(1) > .MuiButtonBase-root > .MuiIconButton-label > .jss10').should('have.value','straw').should('not.be.checked') //Confirming Straw radio is not checked
+        cy.get(':nth-child(2) > .MuiButtonBase-root > .MuiIconButton-label > .jss10').should('have.value','sticks').should('not.be.checked') //Confirming Sticks radio is not checked
+        cy.get(':nth-child(3) > .MuiButtonBase-root > .MuiIconButton-label > .jss10').should('have.value','bricks').should('not.be.checked') //Confirming Bricks radio is not checked
         cy.contains('Is your home located within 1 mile of a body of water?').should('not.exist') //since there is no UX message telling users they need to select an option, i'm validating the header on the next page doesn't show up
     })
 
     it('Customer successfully selects and submits straw building material options and advances to the Water Proximity page', () => {
         //Straw Building Material validation
         cy.contains('Straw').click()
-        cy.get(':nth-child(1) > .MuiButtonBase-root > .MuiIconButton-label > .jss10').should('be.checked') //Confirming Straw radio is checked
+        cy.get(':nth-child(1) > .MuiButtonBase-root > .MuiIconButton-label > .jss10').should('have.value','straw').should('be.checked') //Confirming Straw radio is checked
         cy.contains('Next').click()
         cy.contains('Is your home located within 1 mile of a body of water?') //validating user advances to next step/page
     })
@@ -35,7 +35,7 @@ describe('Hurricane Insurance Building Materials page', () => {
     it('Customer successfully selects and submits sticks building material options and advances to the Water Proximity page', () => {
         //Sticks Building Material validation
         cy.contains('Sticks').click()
-        cy.get(':nth-child(2) > .MuiButtonBase-root > .MuiIconButton-label > .jss10').should('be.checked') //Confirming Sticks radio is checked
+        cy.get(':nth-child(2) > .MuiButtonBase-root > .MuiIconButton-label > .jss10').should('have.value','sticks').should('be.checked') //Confirming Sticks radio is checked
         cy.contains('Next').click()
         cy.contains('Is your home located within 1 mile of a body of water?') //validating user advances to next step/page
     })
@@ -43,7 +43,7 @@ describe('Hurricane Insurance Building Materials page', () => {
     it('Customer successfully selects and submits bricks building material options and advances to the Water Proximity page', () => {
         //Sticks Building Material validation
         cy.contains('Bricks').click()
-        cy.get(':nth-child(3) > .MuiButtonBase-root > .MuiIconButton-label > .jss10').should('be.checked') //Confirming Bricks radio is checked
+        cy.get(':nth-child(3) > .MuiButtonBase-root > .MuiIconButton-label > .jss10').should('have.value','bricks').should('be.checked') //Confirming Bricks radio is checked
         cy.contains('Next').click()
         cy.contains('Is your home located within 1 mile of a body of water?') //validating user advances to next step/page
     })
